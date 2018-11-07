629339bd22b814b1659b4c7afab1a239c39fd9bc
Lourenço <l.gomes@winwel.com>
2018-11-07 12:08:21 +0000

    - Files Permisson

--- app/src/main/java/ipca/edjd/apontamentos/MainActivity.java
+++ app/src/main/java/ipca/edjd/apontamentos/MainActivity.java
@@ -1,10 +1,17 @@
 package ipca.edjd.apontamentos;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.BaseBundle;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -16,6 +23,7 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -63,6 +71,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestPermission();
+            } else {
+                Log.i("apontamentos","External storage permission has already been granted.");
+            }
+        }
+
     }
 
     @Override
@@ -118,6 +137,66 @@ public class MainActivity extends AppCompatActivity {
 
             return convertView;
         }
+    }
+
+    private static final int PERMISSION_REQUEST_WRITE_EXSD = 1002;
+
+    private void requestPermission() {
+        if (ActivityCompat.
+                shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.setTitle("Esta app precisa de escrever no cartão!");
+            builder.setMessage("Por favor dê permissão de escrita.");
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    ActivityCompat.requestPermissions(MainActivity.this,                  new String[]{
+                                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                            },
+                            PERMISSION_REQUEST_WRITE_EXSD);
+                }
+            });
+            builder.show();
+        } else {
+            ActivityCompat.requestPermissions(this,                  new String[]{
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    },
+                    PERMISSION_REQUEST_WRITE_EXSD);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        if (requestCode == PERMISSION_REQUEST_WRITE_EXSD) {
+            if(ActivityCompat.
+                    checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(MainActivity.this, "Premission Granted", Toast.LENGTH_SHORT).show();
+
+
+            } else {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Permission not Granted");
+                builder.setMessage("Go to settings to change permisson");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                    }
+
+                });
+                builder.show();
+
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
     }
 
 }
